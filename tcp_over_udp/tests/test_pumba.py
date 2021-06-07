ec2b2551cfaa4c9035ef72ef333270aa4121826f
pufit <pufit@yandex.ru>
2021-06-07 20:28:55 +0300

    Small style refactoring for pumba test

--- tcp_over_udp/tests/test_pumba.py
+++ tcp_over_udp/tests/test_pumba.py
@@ -11,32 +11,30 @@ def build_docker():
     compose.down().call()
 
 
-def test_loss():
+@pytest.fixture(scope='function')
+def compose():
     compose = DockerCompose()
+    yield compose
+    compose.down().call()
+
+
+def test_loss(compose):
     compose.up(abort_on_container_exit=True, detach=False).call('main-server', 'main-client', 'loss').check_returncode()
-    compose.down().call()
 
 
-def test_delay():
-    compose = DockerCompose()
+def test_delay(compose):
     compose.up(abort_on_container_exit=True, detach=False).call('main-server', 'main-client', 'delay').check_returncode()
-    compose.down().call()
 
 
-def test_duplicate():
-    compose = DockerCompose()
+def test_duplicate(compose):
     compose.up(abort_on_container_exit=True, detach=False).call('main-server', 'main-client', 'duplicate').check_returncode()
-    compose.down().call()
 
 
-def test_corrupt():
-    compose = DockerCompose()
+def test_corrupt(compose):
     compose.up(abort_on_container_exit=True, detach=False).call('main-server', 'main-client', 'corrupt').check_returncode()
-    compose.down().call()
 
 
-def test_all():
-    compose = DockerCompose()
+def test_all(compose):
     compose.up(abort_on_container_exit=True, detach=False).call(
         'main-server',
         'main-client',
@@ -45,4 +43,3 @@ def test_all():
         'duplicate',
         'corrupt',
     ).check_returncode()
-    compose.down().call()
